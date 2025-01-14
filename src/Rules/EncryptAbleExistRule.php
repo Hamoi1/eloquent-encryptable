6fdfd13336fe7bc1eedbdab1e4d5c5f986babc6b
Muhammad Esmael <ihama9728@gmail.com>
2025-01-14 23:46:09 +0300

    Add custom validation rules for encrypted data existence and uniqueness

---
+++ src/Rules/EncryptAbleExistRule.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Hamoi1\EloquentEncryptAble\Rules;
+
+use Hamoi1\EloquentEncryptAble\Services\EloquentEncryptAbleService;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Support\Facades\DB;
+
+class EncryptAbleExistRule  implements ValidationRule
+{
+    protected $table;
+    protected $column;
+    protected $except;
+
+    public function __construct($table, $column, $except = [])
+    {
+        $this->table = $table;
+        $this->column = $column;
+        $this->except = ['column' => data_get($except, 'column'), 'value' => data_get($except, 'value')];
+    }
+
+    public function validate(string $attribute, mixed $value, \Closure $fail): void
+    {
+        $encryptedValue = app(EloquentEncryptAbleService::class)->encrypt($value);
+        $query = DB::table($this->table)->where($this->column, $encryptedValue);
+
+        if (data_get($this->except, 'column') && data_get($this->except, 'value')) {
+            $query->where($this->except['column'], '!=', $this->except['value']);
+        }
+
+        if (!$query->exists()) {
+            $fail("The $attribute does not exist.");
+        }
+    }
+}
