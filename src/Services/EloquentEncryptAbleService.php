135c66b9ac91334c159440c68061c6c5b41f4974
Muhammad Esmael <ihama9728@gmail.com>
2025-01-14 22:37:54 +0300

    Enhance EloquentEncryptAbleService to support multilingual encryption with Kurdish/Arabic characters and improve character handling methods

--- src/Services/EloquentEncryptAbleService.php
+++ src/Services/EloquentEncryptAbleService.php
@@ -7,42 +7,69 @@ use InvalidArgumentException;
 class EloquentEncryptAbleService
 {
     /**
-     * The alphabet used for the Hill cipher.
+     * The alphabet used for encryption and decryption support English and Kurdish/Arabic characters.
      *
      * @var string
      */
-    private const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    private const ALPHABET = [
+        'en' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+        'ckb_and_ar' => 'ابتثجحخدذرزسشصضطظعغفقكلمنهويکچڕگۆێەڵئءآأإةؤی',
+    ];
 
+    /**
+     * The special characters skipped during encryption and decryption.
+     *
+     * @var string
+     */
     private const SPECIAL_CHARACTERS = "!@#$%^&*()_+-=[]{}|;':,.<>?/`~";
 
     /**
-     * Returns the size of the alphabet.
+     * Returns the size of the alphabet for the specified language.
+     * 
+     * @param  string  $lang  The language to get the size of the alphabet for.
      */
-    private function getSize(): int
+    private function getSize(string $lang = 'en'): int
     {
-        return mb_strlen(self::ALPHABET, 'UTF-8');
+        return mb_strlen(self::ALPHABET[$lang], 'UTF-8');
     }
 
     /**
-     * Converts a letter to number
+     * Returns the language of a character.
+     *
+     * @param  string  $char  The character to get the language of.
+     * @return string The language of the character.
+     */
+    private function getLanguageOfChar(string $char): string
+    {
+        if (preg_match('/[a-zA-Z]/', $char)) {
+            return 'en';
+        } else {
+            return 'ckb_and_ar';
+        }
+    }
+
+    /**
+     * Converts a letter to number representation using the specified language.
      *
      * @param  string  $letter  The letter to convert.
+     * @param  string  $lang  The language to use.
      * @return int The numerical representation of the letter.
      */
-    private function charToNumber(string $letter): int
+    private function charToNumber(string $letter, string $lang = 'en'): int
     {
-        return array_search($letter, mb_str_split(self::ALPHABET), true);
+        return array_search($letter, mb_str_split(self::ALPHABET[$lang]), true);
     }
 
     /**
-     * Converts a number to a letter.
+     * Converts a number to a letter using the specified language.
      *
      * @param  int  $number  The number to convert.
+     * @param  string  $lang  The language to use.
      * @return string The corresponding letter.
      */
-    private function numberToChar(int $number): string
+    private function numberToChar(int $number, string $lang = 'en'): string
     {
-        return array_values(mb_str_split(self::ALPHABET))[$number];
+        return array_values(mb_str_split(self::ALPHABET[$lang]))[$number];
     }
 
     /**
@@ -64,24 +91,27 @@ class EloquentEncryptAbleService
     }
 
     /**
-     * Calculates the modular inverse of a 2x2 or 3x3 matrix.
-     *
+     * Calculates the modular inverse of a 2x2 or 3x3 matrix
+     *  using the determinant of the matrix and the size of the alphabet.
+     * 
      * @param  array  $keyMatrix  The key matrix.
+     * @param  array  $languages  The languages of the characters in the matrix.
      * @return array The inverse of the matrix.
      */
-    private function inverseMatrix(array $keyMatrix): array
+    private function inverseMatrix(array $keyMatrix, array $languages = []): array
     {
-        $determinant = $this->determinantMatrix($keyMatrix) % self::getSize();
+        $size = self::getSize(data_get($languages, 0, 'en')); // get the size of the alphabet
+        $determinant = $this->determinantMatrix($keyMatrix) %  $size;
 
         if ($determinant < 0) {
-            $determinant += self::getSize();
+            $determinant +=  $size;
         }
 
         $scalar = 0;
 
         // Find modular inverse of determinant
-        for ($i = 0; $i < self::getSize(); $i++) {
-            if (($i * $determinant) % self::getSize() == 1) {
+        for ($i = 0; $i <  $size; $i++) {
+            if (($i * $determinant) %  $size == 1) {
                 $scalar = $i;
                 break;
             }
@@ -91,8 +121,8 @@ class EloquentEncryptAbleService
             count($keyMatrix) == 2
         ) {
             return [
-                [($keyMatrix[1][1] * $scalar) % (self::getSize()), ((self::getSize()) - ($keyMatrix[0][1] * $scalar) % (self::getSize())) % (self::getSize())],
-                [((self::getSize()) - ($keyMatrix[1][0] * $scalar) % (self::getSize())) % (self::getSize()), ($keyMatrix[0][0] * $scalar) % (self::getSize())],
+                [($keyMatrix[1][1] * $scalar) % ($size), (($size) - ($keyMatrix[0][1] * $scalar) % ($size)) % ($size)],
+                [(($size) - ($keyMatrix[1][0] * $scalar) % ($size)) % ($size), ($keyMatrix[0][0] * $scalar) % ($size)],
             ];
         } elseif (count($keyMatrix) == 3) {
             $a = $keyMatrix[0][0];
@@ -115,9 +145,9 @@ class EloquentEncryptAbleService
                 [$d * $h - $e * $g, $b * $g - $a * $h, $a * $e - $b * $d],
             ];
 
-            return $inverse = array_map(function ($row) use ($scalar) {
-                return array_map(function ($value) use ($scalar) {
-                    return ($value * $scalar) % self::getSize();
+            return $inverse = array_map(function ($row) use ($scalar, $size) {
+                return array_map(function ($value) use ($scalar, $size) {
+                    return ($value * $scalar) %  $size;
                 }, $row);
             }, $adj);
         } else {
@@ -126,20 +156,21 @@ class EloquentEncryptAbleService
     }
 
     /**
-     * Performs matrix multiplication with a 2x2 or 3x3 key matrix and a vector.
+     * Performs matrix multiplication with a 2x2 or 3x3 key matrix and a vector
+     * using the size of the alphabet.
      *
      * @param  array  $keyMatrix  The key matrix.
      * @param  array  $vector  The vector to multiply.
+     * @param  array  $languages  The languages of the characters in the matrix.
      * @return array The resulting vector.
      */
-    private function vectorMatrixMultiplication(array $keyMatrix, array $vector): array
+    private function vectorMatrixMultiplication(array $keyMatrix, array $vector, array $languages = []): array
     {
         $result = array_fill(0, count($keyMatrix), 0);
-
         for ($i = 0; $i < count($keyMatrix); $i++) {
             for ($j = 0; $j < count($keyMatrix); $j++) {
                 $result[$i] += $keyMatrix[$i][$j] * $vector[$j];
-                $result[$i] %= self::getSize();
+                $result[$i] %= self::getSize(data_get($languages, $i, 'en')); // get the size of the alphabet for the language
             }
         }
 
@@ -167,8 +198,8 @@ class EloquentEncryptAbleService
             $keyMatrix = $this->generateRandomInvertibleKeyMatrix(count($keyMatrix));
 
             throw new InvalidArgumentException(
-                'Invalid Hill cipher key matrix in .env file.'.PHP_EOL.'The key matrix is not invertible. A random invertible key matrix has been generated.'
-                    .PHP_EOL.'Please update the key matrix in the .env file with the following:'.PHP_EOL.json_encode($keyMatrix)
+                'Invalid Hill cipher key matrix in .env file.' . PHP_EOL . 'The key matrix is not invertible. A random invertible key matrix has been generated.'
+                    . PHP_EOL . 'Please update the key matrix in the .env file with the following:' . PHP_EOL . json_encode($keyMatrix)
             );
         }
 
@@ -232,7 +263,7 @@ class EloquentEncryptAbleService
      */
     private function isSpaceOrSpecialCharacter(string $char): bool
     {
-        return preg_match('/[\s\d'.preg_quote(self::SPECIAL_CHARACTERS, '/').']/u', $char);
+        return preg_match('/[\s\d' . preg_quote(self::SPECIAL_CHARACTERS, '/') . ']/u', $char);
     }
 
     /**
@@ -243,6 +274,19 @@ class EloquentEncryptAbleService
     private function getDummyCharacter(): string
     {
         return '_';
+    }
+
+    /**
+     * Returns the languages of the characters in an array.
+     *
+     * @param  array  $chars  The characters.
+     * @return array The languages of the characters.
+     */
+    private function getLanguages(array $chars): array
+    {
+        return array_map(function ($char) {
+            return $this->getLanguageOfChar($char);
+        }, $chars);
     }
 
     /**
@@ -268,6 +312,8 @@ class EloquentEncryptAbleService
         // Split the word into blocks of by the size of the key matrix 2x2 or 3x3
         foreach (mb_str_split($word, count($keyMatrix)) as $chars) {
             $chars = mb_str_split($chars); // convert to array
+            // Determine the language of the characters
+            $languages =  $this->getLanguages($chars);
             // check the characters are spaces or numbers
             $checkSpecialCharactersOrSpace = array_map(function ($char) {
                 return $this->isSpaceOrSpecialCharacter($char);
@@ -278,13 +324,13 @@ class EloquentEncryptAbleService
             } else {
                 // Determine if the characters are uppercase or lowercase
                 $isUpperCase = array_map('ctype_upper', $chars);
-                $vector = array_map(function ($char) {
-                    return $this->charToNumber(mb_strtoupper($char)); // convert to uppercase
-                }, $chars);
+                $vector = array_map(function ($char, $lang) {
+                    return $this->charToNumber(mb_strtoupper($char), $lang); // get the numerical representation of the character by specific language
+                }, $chars, $languages);
                 // Encrypt the vector using the key matrix
-                $encryptedVector = $this->vectorMatrixMultiplication($keyMatrix, $vector);
+                $encryptedVector = $this->vectorMatrixMultiplication($keyMatrix, $vector, $languages); // get the encrypted vector
                 foreach ($encryptedVector as $index => $num) {
-                    $encryptedChar = $this->numberToChar($num);
+                    $encryptedChar = $this->numberToChar($num, $languages[$index]); // get the character from the numerical representation by specific language
                     // Convert back to original case
                     $encryptedWord .= $isUpperCase[$index] ? mb_strtoupper($encryptedChar) : mb_strtolower($encryptedChar);
                 }
@@ -322,6 +368,8 @@ class EloquentEncryptAbleService
 
         foreach (mb_str_split($encryptedWord, count($keyMatrix)) as $chars) {
             $chars = mb_str_split($chars); // convert to array
+            // Determine the language of the characters
+            $languages =  $this->getLanguages($chars);
             $checkSpecialCharacters = array_map(function ($char) {
                 return $this->isSpaceOrSpecialCharacter($char);
             }, $chars);
@@ -331,13 +379,13 @@ class EloquentEncryptAbleService
             } else {
                 // Determine if the characters are uppercase or lowercase
                 $isUpperCase = array_map('ctype_upper', $chars);
-                $vector = array_map(function ($char) {
-                    return $this->charToNumber(mb_strtoupper($char)); // convert to uppercase
-                }, $chars);
+                $vector = array_map(function ($char, $lang) {
+                    return $this->charToNumber(mb_strtoupper($char), $lang); // get the numerical representation of the character by specific language
+                }, $chars, $languages);
                 // Decrypt the vector using the key matrix and convert back to original case
-                $decryptedVector = $this->vectorMatrixMultiplication($this->inverseMatrix($keyMatrix), $vector);
+                $decryptedVector = $this->vectorMatrixMultiplication($this->inverseMatrix($keyMatrix, $languages), $vector, $languages); // get the decrypted vector
                 foreach ($decryptedVector as $index => $num) {
-                    $decryptedChar = $this->numberToChar($num);
+                    $decryptedChar = $this->numberToChar($num, $languages[$index]); // get the character from the numerical representation by specific language
                     $decryptedWord .= $isUpperCase[$index] ? mb_strtoupper($decryptedChar) : mb_strtolower($decryptedChar);
                 }
             }
